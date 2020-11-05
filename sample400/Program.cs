89c36e52a0d7e3310b1acddbd4e5af495dca3dd7
ilkerokutman@gmail.com <atom1119>
2020-11-05 11:36:51 +0300

    Initial check-in of module sample400

---
+++ sample400/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sample400
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 100 ile 400 arasindaki sayilardan tum rakamlari cift olanlari
+            // ve bunlarin sayisini bulan bir program yaziniz
+
+            int qty = 0; // kac sayi bu kosulu sagliyor
+            int startingNumber = 100; // kactan basliyor
+            int endingNumber = 400; // kacta bitiyor
+
+            Console.WriteLine($"{startingNumber} ile {endingNumber} arasindaki sayilardan tum rakamlari cift olan sayilar: ");
+
+            for (int i = startingNumber; i <=endingNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // sayi ikiye bolunebiliyor
+                    
+                    int digit10 = i / 10; // onlar basamagi bulunuyor
+
+                    if (digit10 % 2 == 0)
+                    {
+                        // onlar basamagi ikiye bolunebiliyor
+                        int digit100 = i / 100; // yuzler basamagi bulunuyor
+
+                        if (digit100 % 2 == 0)
+                        {
+                            // yuzler basamagi ikiye bolunebiliyor
+                            Console.WriteLine(i);
+                            qty++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Toplam sayi aded: {qty}");
+        }
+    }
+}
